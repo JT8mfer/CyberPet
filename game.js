1ec4f694454cea74cc85e3e4f37129adf7e26e3d
jordan <jthomasw88>
2021-04-11 16:35:56 +0100

    commit

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const petBoxSingular = document.getElementById("petBoxSingular");
 const buttonRevivePet = document.getElementById("buttonRevivePet");
 
 //petIcon//
-document.getElementById("petIcon").src = "/img/cyberPetDogNeutral.png";
+document.getElementById("petIcon").src = "./img/cyberPetDogHappy.png";
 
 //Interacting with Pet//
 buttonPlayWith.addEventListener("click", () => {
@@ -21,82 +21,81 @@ buttonPlayWith.addEventListener("click", () => {
 });
 buttonPlayWith.addEventListener("mousedown", () => {
   //makes dog happy when interacted with//
-  petIcon.src = "/img/cyberPetDogHappy.png";
+  petIcon.src = "./img/cyberPetDogHappy.png";
 });
 buttonPlayWith.addEventListener("mouseup", () => {
   //returns dog to neutral state on mouse release//
-  petIcon.src = "/img/cyberPetDogNeutral.png";
+  petIcon.src = "./img/cyberPetDogNeutral.png";
 });
 buttonFeed.addEventListener("click", () => {
   //increases value of hunger level when clicked//
   objectLvlHunger.value += 20;
 });
 buttonFeed.addEventListener("mousedown", () => {
-  petIcon.src = "/img/cyberPetDogHappy.png";
+  petIcon.src = "./img/cyberPetDogHappy.png";
 });
 buttonFeed.addEventListener("mouseup", () => {
-  petIcon.src = "/img/cyberPetDogNeutral.png";
+  petIcon.src = "./img/cyberPetDogNeutral.png";
 });
 buttonWater.addEventListener("click", () => {
   //increases value of thirst level when clicked//
   objectLvlThirst.value += 20;
 });
 buttonWater.addEventListener("mousedown", () => {
-  petIcon.src = "/img/cyberPetDogHappy.png";
+  petIcon.src = "./img/cyberPetDogHappy.png";
 });
 buttonWater.addEventListener("mouseup", () => {
-  petIcon.src = "/img/cyberPetDogNeutral.png";
+  petIcon.src = "./img/cyberPetDogNeutral.png";
 });
 buttonWalk.addEventListener("click", () => {
   //increases value of walks level when clicked//
   objectLvlWalks.value += 20;
 });
 buttonWalk.addEventListener("mousedown", () => {
-  petIcon.src = "/img/cyberPetDogHappy.png";
+  petIcon.src = "./img/cyberPetDogHappy.png";
 });
 buttonWalk.addEventListener("mouseup", () => {
-  petIcon.src = "/img/cyberPetDogNeutral.png";
+  petIcon.src = "./img/cyberPetDogNeutral.png";
 });
 buttonKillPet.addEventListener("click", () => {
-  //to send dog to farm//
   objectLvlPlay.value = 0;
   objectLvlHunger.value = 0;
   objectLvlThirst.value = 0;
   objectLvlWalks.value = 0;
-  petIcon.src = "/img/cyberPetDogRip.png";
+  petIcon.src = "./img/cyberPetDogRip.png";
   petBoxSingular.style.background = "grey";
   buttonKillPet.style.display = "none";
   buttonRevivePet.style.display = "block";
 });
 buttonRevivePet.addEventListener("click", () => {
   //to revive dog//
-  objectLvlPlay.value = 1000;
-  objectLvlHunger.value = 100;
-  objectLvlThirst.value = 100;
-  objectLvlWalks.value = 100;
-  petIcon.src = "/img/cyberPetDogNeutral.png";
+  objectLvlPlay.value = 100;
+  objectLvlHunger.value = 10;
+  objectLvlThirst.value = 10;
+  objectLvlWalks.value = 10;
+  petIcon.src = "./img/cyberPetDogRevive.png";
   petBoxSingular.style.background = " #F4CEC7";
   buttonKillPet.style.display = "block";
   buttonRevivePet.style.display = "none";
 });
 
-objectLvlPlay.value = 100;
-objectLvlHunger.value = 100;
-objectLvlThirst.value = 100;
-objectLvlWalks.value = 100;
+objectLvlPlay.value = 10;
+objectLvlHunger.value = 10;
+objectLvlThirst.value = 10;
+objectLvlWalks.value = 10;
 
 let play = setInterval(() => {
   objectLvlPlay.value--;
-}, 1000);
+}, 100);
 let hunger = setInterval(() => {
   objectLvlHunger.value--;
-}, 1100);
+}, 110);
 let thirst = setInterval(() => {
   objectLvlThirst.value--;
-}, 1200);
+}, 120);
 let walks = setInterval(() => {
   objectLvlWalks.value--;
-}, 2000);
+}, 200);
 
 if (objectLvlPlay.value == 0) {
   clearInterval(play);
